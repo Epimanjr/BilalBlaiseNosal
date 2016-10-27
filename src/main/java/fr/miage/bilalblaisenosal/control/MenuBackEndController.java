324e7bf5ce5c231fb489f15d8ce07c3f7e0ce80c
Antoine NOSAL <antoine.nosal@gmail.com>
2016-10-27 17:53:00 +0200

    permet d'ajouter une oeuvre depuis le BackEnd

--- src/main/java/fr/miage/bilalblaisenosal/control/MenuBackEndController.java
+++ src/main/java/fr/miage/bilalblaisenosal/control/MenuBackEndController.java
@@ -7,7 +7,13 @@ package fr.miage.bilalblaisenosal.control;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -17,12 +23,30 @@ import javafx.fxml.Initializable;
  */
 public class MenuBackEndController implements Initializable {
 
+    private static String FXML_FOLDER = "/fxml/";
+    private static String WINDOWNAME_PREFIX = "BibalBlaiseNosal - ";
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    private void launch(String fxmlName, String windowName) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(FXML_FOLDER + fxmlName + ".fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle(WINDOWNAME_PREFIX + windowName);
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+    }
+    
+    @FXML
+    private void launch_oeuvre(ActionEvent event) throws Exception {
+        this.launch("Oeuvre", "Gestion des oeuvres");
+    }
     
 }
